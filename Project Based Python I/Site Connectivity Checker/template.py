ec0f3ce1d7fb1ad0047c661734167da9fd7ac794
Khosro HosseinAbadi <khosro.h2011@gmail.com>
2023-04-25 19:18:21 +0330

    add Site Conectivity Checker

---
+++ Project Based Python I/Site Connectivity Checker/template.py
@@ -0,0 +1,15 @@
+import urllib.request
+import tkinter as tk
+
+
+def test_connectivity():
+    # Some innitial values
+    # Make GUI 
+
+    def check_url():
+        # Send http request and get its status code.
+        # If code is 200 it means that website is available.
+        # Show the proper message
+
+if __name__ == '__main__':
+    test_connectivity()
