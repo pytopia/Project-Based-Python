3156370191ec14732df4e03952cc5cb470d9d137
Khosro HosseinAbadi <khosro.h2011@gmail.com>
2023-04-27 22:52:09 +0330

    add Happy Numbers

---
+++ Project Based Python I/Happy Numbers/solutions/solution-1.py
@@ -0,0 +1,38 @@
+def get_digits(number):
+    digits = []
+    while number:
+        digits.append(number % 10)
+        number //= 10
+    digits.reverse()
+    return digits
+
+#---------------------------------------
+def is_happy_number(number):
+    previous_numbers = []
+    while True:
+        digits = get_digits(number)
+        sum_of_squared_digits = sum(list(map(lambda x: x **2, digits)))
+        if sum_of_squared_digits == 1:
+            return True
+        elif sum_of_squared_digits in previous_numbers:
+            return False
+        else:
+            number = sum_of_squared_digits
+            previous_numbers.append(number)
+            
+#----------------------------------------------
+def print_happy_number(number):
+    happy_numbers = []
+    count = 0
+    while count < 8:
+        if is_happy_number(number):
+            happy_numbers.append(number)
+            count += 1
+        number += 1
+    return happy_numbers
+
+#---------------------------------------------
+if __name__ == '__main__':
+    number = int(input('Enter the number: '))
+
+    print(print_happy_number(number))
---
+++ Project Based Python I/Happy Numbers/template.py
@@ -0,0 +1,13 @@
+def get_digits(number):
+    pass
+
+def is_happy_number(number):
+    pass
+
+def print_happy_number(number):
+    pass
+
+if __name__ == '__main__':
+    number = int(input('Enter the number: '))
+
+    print(print_happy_number(number))
