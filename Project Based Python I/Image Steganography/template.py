8d279187a7af557415909c85cc2e5032fbf2b939
Khosro HosseinAbadi <khosro.h2011@gmail.com>
2023-05-01 10:51:04 +0330

    add Message Encryption

---
+++ Project Based Python I/Image Steganography/template.py
@@ -0,0 +1,18 @@
+# Source Code Link:
+# https://techvidvan.com/tutorials/python-image-steganography/
+
+# Importing modules
+
+
+def Decode():
+    # Decoding functione
+    pass
+
+
+def Encode():
+    # Encoding function
+    pass
+
+# Initializing the screen
+
+# Creating buttons
