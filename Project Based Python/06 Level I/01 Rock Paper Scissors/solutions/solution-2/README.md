dc2724620caa5840de9489c245f6c1a9166964de
Ali Hejazi <hejazizo@Alis-MacBook-Pro.local>
2023-10-30 21:20:36 -0400

    Update content

---
+++ Project Based Python/06 Level I/01 Rock Paper Scissors/solutions/solution-2/README.md
@@ -0,0 +1,27 @@
+# Rock Paper Scissors Game - Python Project
+
+This project is a Python-based implementation of the popular game "Rock, Paper, Scissors", executed from a functional programming perspective. You have the opportunity to face off against a computer opponent, which makes its moves based on random choice. The game continues until you decide to quit. 
+
+## Project Structure
+```
+rock-paper-scissors/
+│
+├── src/
+│   ├── main.py - File with the game's functions and play logic
+│
+├── README.md - This file
+```
+
+## Requirements
+1. Python 3.7+
+
+## Running the Project
+To run the project:
+
+```python
+python src/main.py
+```
+
+In this solution, the Rock Paper Scissors game logic is implemented using a procedural or functional programming approach. The source code includes functions to get the user's and the computer's choices, determine the winner, and to play the game repeatedly until the user decides to quit.
+
+When you run the script, you'll be prompted to make a choice, after which the computer's choice is randomly generated and the winner is declared. The game will continue to run until you decide to quit.
---
+++ Project Based Python/06 Level I/01 Rock Paper Scissors/solutions/solution-2/src/game.py
@@ -0,0 +1,42 @@
+import random
+
+
+def get_user_choice():
+    """Gets the user's choice."""
+    print("\n1. Rock\n2. Paper\n3. Scissors")
+    user_choice = int(input("Enter your choice: "))
+    while user_choice < 1 or user_choice > 3:
+        user_choice = int(input("Enter a valid choice: "))
+    items = {1: 'rock', 2: 'paper', 3: 'scissors'}
+    return items[user_choice]
+
+
+def get_computer_choice():
+    """Gets the computer's choice."""
+    computer_choice = random.randint(1, 3)
+    items = {1: 'rock', 2: 'paper', 3: 'scissors'}
+    return items[computer_choice]
+
+
+def determine_winner(user_choice, computer_choice):
+    """Determines the winner."""
+    winners = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
+    if user_choice == computer_choice:
+        return "It's a tie"
+    elif winners[user_choice] == computer_choice:
+        return "User wins"
+    else:
+        return "Computer wins"
+
+
+def play():
+    """Plays the game."""
+    user_choice = get_user_choice()
+    computer_choice = get_computer_choice()
+    print("User chose:", user_choice)
+    print("Computer chose:", computer_choice)
+    print(determine_winner(user_choice, computer_choice))
+
+
+if __name__ == "__main__":
+    play()
