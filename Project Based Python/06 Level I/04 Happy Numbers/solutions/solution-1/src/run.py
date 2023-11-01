0b313a200b1f8621e3c6707cfdfb4f6537e931dc
Ali Hejazi <hejazizo@Alis-MacBook-Pro.local>
2023-11-01 02:27:44 -0400

    Update content

--- Project Based Python/06 Level I/04 Happy Numbers/solutions/solution-1/src/run.py
+++ Project Based Python/06 Level I/04 Happy Numbers/solutions/solution-1/src/run.py
@@ -3,10 +3,10 @@ def is_happy(n: int) -> bool:
     Checks whether a given number is happy or not.
 
     A happy number is a number defined by the following process:
-    Starting with any positive integer, replace the number by the sum 
-    of the squares of its digits in base-ten, and repeat the process 
-    until the number either equals 1 (where it will stay), or it loops 
-    endlessly in a cycle that does not include 1. Those numbers for 
+    Starting with any positive integer, replace the number by the sum
+    of the squares of its digits in base-ten, and repeat the process
+    until the number either equals 1 (where it will stay), or it loops
+    endlessly in a cycle that does not include 1. Those numbers for
     which this process ends in 1 are happy numbers.
 
     :param n: The number to check.
