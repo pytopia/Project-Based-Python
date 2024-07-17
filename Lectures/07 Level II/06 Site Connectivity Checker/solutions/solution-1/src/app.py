fd90ad12046f8c418c42f522a681b703f43744b2
hejazizo <hejazizo@ualberta.ca>
2024-07-17 11:39:11 -0400

    Update content

--- Lectures/07 Level II/06 Site Connectivity Checker/solutions/solution-1/src/app.py
+++ Lectures/07 Level II/06 Site Connectivity Checker/solutions/solution-1/src/app.py
@@ -21,7 +21,10 @@ def format_url(url):
 # Function to check website availability
 def check_site_availability(url):
     try:
-        response = requests.get(url, timeout=5)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        }
+        response = requests.get(url, timeout=5, headers=headers)
         if response.status_code == 200:
             return True
         else:
