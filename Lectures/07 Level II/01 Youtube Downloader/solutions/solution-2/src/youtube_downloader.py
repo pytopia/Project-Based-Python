1ff971760ab0c13869b659f64355d2551b1f8fce
Khosro Hosseinabadi <khosro.h2011@gmail.com>
2024-09-14 18:24:54 +0330

    Bug fix - because of a bug in pytube, use pytubefix instead

--- Lectures/07 Level II/01 Youtube Downloader/solutions/solution-2/src/youtube_downloader.py
+++ Lectures/07 Level II/01 Youtube Downloader/solutions/solution-2/src/youtube_downloader.py
@@ -1,39 +1,61 @@
 import argparse
 
-from pytube import YouTube
+## Because of a bug in pytube, we can use pytubefix instead
+# from pytube import YouTube
+# from pytube.exceptions import VideoUnavailable
+from pytubefix import YouTube
+from pytubefix.exceptions import VideoUnavailable
 from tqdm import tqdm
-# import pytube exceptions
-from pytube.exceptions import VideoUnavailable
-
 
 
 class YouTubeDownloader:
-    def __init__(self, url, quality='highest', output_path='.'):
+    def __init__(self, url, quality="highest", output_path="."):
         self.url = url
         self.quality = quality
         self.output_path = output_path
-        self.yt = YouTube(url, on_progress_callback=self.on_progress, on_complete_callback=self.on_complete)
+        self.yt = YouTube(
+            url,
+            on_progress_callback=self.on_progress,
+            on_complete_callback=self.on_complete,
+        )
         self.pbar = None
 
     def download(self):
         try:
             # If user wants the highest resolution, use 'get_highest_resolution' filter
-            if self.quality == 'highest':
-                video_stream = self.yt.streams.filter(progressive=True, file_extension='mp4').get_highest_resolution()
+            if self.quality == "highest":
+                video_stream = self.yt.streams.filter(
+                    progressive=True, file_extension="mp4"
+                ).get_highest_resolution()
             else:
-                video_stream = self.yt.streams.filter(progressive=True, res=self.quality, file_extension='mp4').first()
+                video_stream = self.yt.streams.filter(
+                    progressive=True, res=self.quality, file_extension="mp4"
+                ).first()
 
             if video_stream is None:
+                available_qualities = [
+                    str(stream.resolution)
+                    for stream in self.yt.streams.filter(
+                        progressive=True, file_extension="mp4"
+                    )
+                ]
                 print(f"No streams found with the specified quality: {self.quality}")
+                print(f"Title: {self.yt.title}")
+                print(f"Available qualities: {available_qualities}")
                 return
 
             # Initialize tqdm progress bar
-            self.pbar = tqdm(total=video_stream.filesize, unit='B', unit_scale=True, desc=self.yt.title)
+            self.pbar = tqdm(
+                total=video_stream.filesize,
+                unit="B",
+                unit_scale=True,
+                desc=self.yt.title,
+            )
 
             # Download the video
             video_stream.download(self.output_path)
 
-        except Exception as e:
+        except VideoUnavailable as e:
             print("An error occurred:", e)
             if self.pbar:
                 self.pbar.close()  # Ensure the progress bar is closed in case of error
@@ -61,16 +83,25 @@ class YouTubeDownloader:
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Download a YouTube video at a specified quality and output path.')
-
-    parser.add_argument('url', help='The YouTube URL to download')
-    parser.add_argument(
-        '-q', '--quality',
-        help='The desired video quality (e.g., 720p, 1080p, highest)',
-        default='highest',
-        type=str
+    parser = argparse.ArgumentParser(
+        description="Download a YouTube video at a specified quality and output path."
     )
-    parser.add_argument('-o', '--output_path', help='The output directory to save the video', default='.', type=str)
+
+    parser.add_argument("url", help="The YouTube URL to download")
+    parser.add_argument(
+        "-q",
+        "--quality",
+        help="The desired video quality (e.g., 720p, 1080p, highest)",
+        default="highest",
+        type=str,
+    )
+    parser.add_argument(
+        "-o",
+        "--output_path",
+        help="The output directory to save the video",
+        default=".",
+        type=str,
+    )
 
     args = parser.parse_args()
 
